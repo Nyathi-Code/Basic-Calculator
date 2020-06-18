fb4f30a9b6c30512ed2d46ce816d1a809a14ef24
Nyathi-Code <61335925+Nyathi-Code@users.noreply.github.com>
2020-06-18 07:22:54 +0200

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,40 @@
+def add(num1, num2): # adding 2 numbers (function)
+  return num1 + num2
+
+def subtract(num1, num2): # subtracting 2 numbers (function)
+  return num1 - num2
+
+def multiply(num1, num2): # multiplying 2 numbers (function)
+  return num1 * num2
+
+def divide(num1, num2): # dividing 2 numbers (function)
+  return num1 / num2
+
+print("Please select an operater -\n" \
+      "1. Add\n" \
+      "2. Subtract\n" \
+      "3. Multiply\n" \
+      "4. Divide\n")
+
+select = float(input("Select operations form 1, 2, 3, 4 :")) #input from user as float
+
+number_1 = float(input("Enter your first number: "))
+number_2 = float(input("Enter your second number: "))
+
+if select == 1:
+  print(number_1, "+", number_2, "=",
+        add(number_1, number_2))
+
+elif select == 2:
+  print(number_1, "-", number_2, "=",
+        subtract(number_1, number_2))
+
+elif select == 3:
+  print(number_1, "*", number_2, "=",
+        multiply(number_1, number_2))
+
+elif select == 4:
+  print(number_1, "/", number_2, "=",
+        divide(number_1, number_2))
+else:
+  print("Invalid input")
